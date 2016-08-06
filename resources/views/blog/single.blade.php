45c47f7f55b635c5cf166b6b688e25caf29313a6
rdbt3028 <rdbt3028@gmail.com>
2016-08-06 17:42:38 +0545

    pusing a single file

--- resources/views/blog/single.blade.php
+++ resources/views/blog/single.blade.php
@@ -4,8 +4,11 @@
 @section('content')
  <div class="row">
      <div class="col-md-8 col-md-offset-2">
+
       <h1>{{ $post->title}}</h1>
        <p>{{ $post->body }}</p>
+       <p>{{ $post->category_id }}</p>
+       {{-- {{ dd('testing',$post->category())}} --}}
        <p>{{ $post->category->name }}</p>
      	
      </div>
